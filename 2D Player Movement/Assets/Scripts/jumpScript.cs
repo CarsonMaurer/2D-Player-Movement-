a2cb994468d64020cbba5ccd2613189507a1f5a3
CarsonMaurer <111007062+CarsonMaurer@users.noreply.github.com>
2022-09-30 11:09:19 -0700

    final changes
    1 jump

--- 2D Player Movement/Assets/Scripts/jumpScript.cs
+++ 2D Player Movement/Assets/Scripts/jumpScript.cs
@@ -7,22 +7,46 @@ public class jumpScript : MonoBehaviour
     public float moveSpeed = 5;
     public float jumpForce = 10;
 
+    public bool isOnGround;
+
+    private Collider2D _myCollider;
+
     private Rigidbody2D _myRB;
     // Start is called before the first frame update
     void Start()
     {
         _myRB = GetComponent<Rigidbody2D>();
+        _myCollider = GetComponent<Collider2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        PlayerMovement();
 
-        _myRB.velocity = new Vector2(horizontalInput * moveSpeed, _myRB.velocity.y);
+        PlayerJump();
 
-        if(Input.GetButtonDown("Jump"))
+       
+    }
+    void PlayerMovement()
+    {
+
+         float horizontalInput = Input.GetAxis("Horizontal");
+         _myRB.velocity = new Vector2(horizontalInput * moveSpeed, _myRB.velocity.y);
+    }
+    void PlayerJump()
+    { 
+        if(_myCollider.IsTouchingLayers(LayerMask.GetMask("ground")))
+        {
+            isOnGround = true;
+        }
+        else 
+        {
+            isOnGround = false;
+        }
+
+         if(Input.GetButtonDown("Jump") && isOnGround)
         {
             _myRB.velocity = new Vector2(_myRB.velocity.x, jumpForce);
         }
